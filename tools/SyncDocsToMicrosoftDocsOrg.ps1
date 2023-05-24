bf6b783a3e37efb33edf86c4b35d1e36d2abe379
Nori Zhang <norizhang@microsoft.com>
2023-05-24 11:05:40 +0800

    Switch to personal account

--- tools/SyncDocsToMicrosoftDocsOrg.ps1
+++ tools/SyncDocsToMicrosoftDocsOrg.ps1
@@ -29,8 +29,11 @@ foreach ($SyncPath in $Config.SyncPath)
 
 $SyncFile = Split-Path $SyncPath -Leaf
 
-git config user.email "65331932+azure-powershell-bot@users.noreply.github.com"
-git config user.name "azure-powershell-bot"
+# git config user.email "65331932+azure-powershell-bot@users.noreply.github.com"
+# git config user.name "azure-powershell-bot"
+
+git config user.email "norizhang@microsoft.com"
+git config user.name "NoriZC"
 
 cd $WorkSpace
 git clone $RepoCloneLink
